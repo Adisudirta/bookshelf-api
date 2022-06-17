15c0c2f9fe775e46ccd59cc9dff96d521de57c53
Adisudirta <yanadisudirta@gmail.com>
2022-06-18 07:44:03 +0800

    Create query params for get all books

--- src/controller/getAllBooksHandler.js
+++ src/controller/getAllBooksHandler.js
@@ -1,8 +1,74 @@
 const books = require('../books');
 
-const getAllBooksHandler = () => {
+const getAllBooksHandler = (request, h) => {
+  const { name: queryName, reading, finished } = request.query;
+
+  if (queryName) {
+    const filteredByName = books
+      .filter((book) => book.name.toUpperCase().includes(queryName.toUpperCase()));
+
+    const filteredPropertyBooks = filteredByName.map((book) => {
+      const { id, name, publisher } = book;
+
+      return { id, name, publisher };
+    });
+
+    return h.response({
+      status: 'success',
+      data: {
+        books: filteredPropertyBooks,
+      },
+    }).code(200);
+  }
+
+  if (reading) {
+    let filteredByReading = [];
+
+    if (reading === '1') {
+      filteredByReading = books.filter((book) => book.reading === true);
+    } else {
+      filteredByReading = books.filter((book) => book.reading === false);
+    }
+
+    const filteredPropertyBooks = filteredByReading.map((book) => {
+      const { id, name, publisher } = book;
+
+      return { id, name, publisher };
+    });
+
+    return h.response({
+      status: 'success',
+      data: {
+        books: filteredPropertyBooks,
+      },
+    }).code(200);
+  }
+
+  if (finished) {
+    let filteredByFinished = [];
+
+    if (finished === '1') {
+      filteredByFinished = books.filter((book) => book.finished === true);
+    } else {
+      filteredByFinished = books.filter((book) => book.finished === false);
+    }
+
+    const filteredPropertyBooks = filteredByFinished.map((book) => {
+      const { id, name, publisher } = book;
+
+      return { id, name, publisher };
+    });
+
+    return h.response({
+      status: 'success',
+      data: {
+        books: filteredPropertyBooks,
+      },
+    }).code(200);
+  }
+
   if (books) {
-    const filteredInformationBooks = books.map((book) => {
+    const filteredPropertyBooks = books.map((book) => {
       const { id, name, publisher } = book;
 
       return {
@@ -13,7 +79,7 @@ const getAllBooksHandler = () => {
     return {
       status: 'success',
       data: {
-        books: filteredInformationBooks,
+        books: filteredPropertyBooks,
       },
     };
   }
